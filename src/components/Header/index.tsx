c6d0c28ebcb544ec734f4d591ab5647d5dae7ad1
giovanifranz <giovanifranz151@gmail.com>
2021-12-01 22:46:53 -0300

    first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <Image src="/spacetraveling.svg" height="23px" width="40px" />
+        <h1>
+          spacetraveling<span>.</span>
+        </h1>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { GetStaticProps } from 'next';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -24,13 +31,75 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <main className={commonStyles.container}>
+      <section className={styles.content}>
+        <article className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <div>
+              <FiCalendar size={20} />
+              <p>19 Abr 2021</p>
+            </div>
+            <div>
+              <FiUser size={20} />
+              <p>Jose Oliveira</p>
+            </div>
+          </div>
+        </article>
+        <article className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.info}>
+            <div>
+              <FiCalendar size={20} />
+              <p>19 Abr 2021</p>
+            </div>
+            <div>
+              <FiUser size={20} />
+              <p>Jose Oliveira</p>
+            </div>
+          </div>
+        </article>
+        <button>Carregar mais posts</button>
+      </section>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: post.data.author,
+      },
+      first_publication_date: format(
+        new Date(post.last_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  console.log(posts);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
